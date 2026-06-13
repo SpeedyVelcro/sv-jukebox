4a45ec9dd219a7691d8adce0bc7ed4aed3915350
SpeedyVelcro <32414336+SpeedyVelcro@users.noreply.github.com>
2026-06-13 07:22:27 +0100

    Implement aspect ratio container fix

---
+++ addons/sv_jukebox/fix/aspect_ratio_container.gd
@@ -0,0 +1,17 @@
+class_name AspectRatioContainerJukeboxFix
+extends AspectRatioContainer
+# See https://github.com/godotengine/godot/issues/75169
+# NB: This workaround prevents overwrites any custom_minimum_size so does not
+# work as a general solution; only for our use case in SV Jukebox.
+
+
+# Override
+func _notification(what: int) -> void:
+	match what:
+		NOTIFICATION_SORT_CHILDREN:
+			var x := 0.0
+			var y := 0.0
+			for child in get_children():
+				x = max(x, child.size.x)
+				y = max(y, child.size.y)
+			custom_minimum_size = Vector2(x, y)
